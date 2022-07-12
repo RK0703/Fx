3d242be5a7eec5bb776a4d214478093a35c58444
RK0703 <ravikiran@fourthfrontier.com>
2022-07-12 14:07:34 +0530

    Failuer Analysis

--- frontierX/src/main/java/com/frontierX/BaseSettings/FxSettings.java
+++ frontierX/src/main/java/com/frontierX/BaseSettings/FxSettings.java
@@ -29,7 +29,7 @@ public abstract class FxSettings //extends TestListenerAdapter
 		else
 		{
 			String chromeKey = "webdriver.chrome.driver";
-			String chromeDriverPath = "./drivers/chromedriverUbuntu.exe";
+			String chromeDriverPath = "/var/lib/jenkins/workspace/Demo/frontierX/drivers/chromedriverUbuntu";
 			
 			System.setProperty(chromeKey, chromeDriverPath);
 			System.out.println("Launching Chrome in Environment : " +FxUtilities.getOperatingSystemSystemUtils());
